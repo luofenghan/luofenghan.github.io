321762804a91111683b103ab1c4d823e9848db40
luofenghan <luofenghan_cwc@163.com>
2017-06-15 13:20:13 +0800

    Site updated: 2017-06-15 13:20:10

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Tue Jun 13 2017 19:51:11 GMT+0800 (马来西亚半岛标准时间)
+// build time:Thu Jun 15 2017 13:18:12 GMT+0800 (马来西亚半岛标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Tue Jun 13 2017 19:51:10 GMT+0800 (马来西亚半岛标准时间)
+// build time:Thu Jun 15 2017 13:18:11 GMT+0800 (马来西亚半岛标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
