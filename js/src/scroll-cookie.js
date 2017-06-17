bd4323f4af4f2400720ea33a88c0ca7dad14cf07
luofenghan <luofenghan_cwc@163.com>
2017-06-17 18:01:17 +0800

    Site updated: 2017-06-17 18:01:15

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sat Jun 17 2017 17:48:22 GMT+0800 (马来西亚半岛标准时间)
+// build time:Sat Jun 17 2017 18:00:59 GMT+0800 (马来西亚半岛标准时间)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
