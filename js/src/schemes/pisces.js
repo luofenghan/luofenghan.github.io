e88cf09093b2243f5e8943c24420f7d4f14162a6
luofenghan <luofenghan_cwc@163.com>
2017-07-05 13:13:56 +0800

    Site updated: 2017-07-05 13:13:48

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Sun Jun 18 2017 16:59:47 GMT+0800 (马来西亚半岛标准时间)
+// build time:Wed Jul 05 2017 13:11:11 GMT+0800 (马来西亚半岛标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
